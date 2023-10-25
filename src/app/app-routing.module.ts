b7a8dfaf80c02b5a88977bb476b6957f7bebde8c
v-i-vek <vivek.yadav@volansys.com>
2023-10-25 17:25:48 +0530

    navigated the otp page to the delivery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DeliveryComponent } from './delivery/delivery.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login-signup/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { VerificationComponent } from './verification/verification.component';
 
 const routes: Routes = [
+  {path:'',component:DeliveryComponent},
   {path:'login', component:LoginComponent},
-  {path:'delivery',component:DeliveryComponent},
-  {path:'verify',component:VerificationComponent}
+  {path:'verify',component:VerificationComponent},
+  {path:'home',component:HomePageComponent}
 ];
 
 @NgModule({
--- src/app/verification/verification.component.ts
+++ src/app/verification/verification.component.ts
@@ -3,39 +3,39 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ServicesService } from '../Services/signUp.service';
 
-
 @Component({
   selector: 'app-verification',
   templateUrl: './verification.component.html',
-  styleUrls: ['./verification.component.scss']
+  styleUrls: ['./verification.component.scss'],
 })
 export class VerificationComponent implements OnInit {
-  constructor(private route: ActivatedRoute,private http:ServicesService){}
-  Id:any =''
-  email:any = ''
+  constructor(private route: ActivatedRoute, private http: ServicesService, private router: Router) {}
+  Id: any = '';
+  email: any = '';
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe(params => this.Id = params.getAll('id'));
-   this.email  = this.Id[0]
-    
+    this.route.queryParamMap.subscribe(
+      (params) => (this.Id = params.getAll('id'))
+    );
+    this.email = this.Id[0];
   }
+
+
 
   otpAuth = new FormGroup({
     otp: new FormControl(''),
-    email:new FormControl('')
+    email: new FormControl(''),
   });
 
-
-otpVerify(value:any){
-  this.otpAuth.value.email = this.email
-this.http.otpAuth(value).subscribe({
-  next:(res)=>{
-    alert("your email is verifed")
-
-  },
-  error:(err)=>{
-    alert("wrong otp")
+  otpVerify(value: any) {
+    this.otpAuth.value.email = this.email;
+    this.http.otpAuth(value).subscribe({
+      next: (res) => {
+        alert('your email is verifed');
+        this.router.navigate([''])
+      },
+      error: (err) => {
+        alert('wrong otp');
+      },
+    });
   }
-})
-}
- 
 }
